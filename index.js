b0b3ac6c03cc3690e0a31539c462cd9e665dbf5e
janusonyte <janusonytegabriele@gmail.com>
2023-04-14 09:50:38 +0100

    ex. 2 & 3

--- index.js
+++ index.js
@@ -2,34 +2,55 @@
 console.log("Uzduotis #1:")
 
 function laikas(h1, min1, h2, min2){
-    let startTime = (h1 * 60) + min1; //gaunam minutes is starto laiko
-    let endTime = (h2 * 60) + min2; //gaunam minutes is pabaigos laiko
+    startTime = (h1 * 60) + min1; //gaunam minutes is starto laiko
+    endTime = (h2 * 60) + min2; //gaunam minutes is pabaigos laiko
         if(startTime < endTime){
             t = endTime - startTime;
         } else {
             t = (1440 - startTime) + endTime;
         }
-    let calcTimeHours = t/60; //verciam tarpini laika i valandas
-    let roundedHours = Math.floor(calcTimeHours); //apvalinam valandas iki artimiausio sveiko skaiciaus (zemyn)
-    let calcTimeMins = (calcTimeHours - roundedHours) * 60; //atimam is tarpiniu valandu apvalintas valandas ir dauginam is 60, kad gautume minutes
-    let roundedMins = Math.round(calcTimeMins);//apvalinam minutes iki artimiausio sveiko skaiciaus 
+    calcTimeHours = t/60; //verciam tarpini laika i valandas
+    roundedHours = Math.floor(calcTimeHours); //apvalinam valandas iki artimiausio sveiko skaiciaus (zemyn)
+    calcTimeMins = (calcTimeHours - roundedHours) * 60; //atimam is tarpiniu valandu apvalintas valandas ir dauginam is 60, kad gautume minutes
+    roundedMins = Math.round(calcTimeMins);//apvalinam minutes iki artimiausio sveiko skaiciaus 
 
     return roundedHours + `h ir ` + roundedMins + `min`; 
 }
 
 console.log(laikas(15,10,14,0))
+console.log(laikas(14,10,15,0))
 
+console.clear();
 
 //Uzduotis #2:
-// console.log("Uzduotis #2:")
+console.log("Uzduotis #2:")
 
+function armstrong() {
+    let arr = []
+    for(let i = 100; i < 1000; i++){
+        let stringNum = i.toString();
+        if(i === stringNum[0]**3 + stringNum[1]**3 + stringNum[2]**3 ) {
+            arr.push(stringNum);
+        }
+    }
+    return arr;
+}
+
+console.log(armstrong());
+console.clear();
 
 //Uzduotis #3:
-// console.log("Uzduotis #3:")
+console.log("Uzduotis #3:")
 
+function distanceBetweenTwoCoords(x1, y1, x2, y2){
+    return Math.sqrt(((x2-x1)**2)+((y2-y1)**2));
+}
+
+console.log(distanceBetweenTwoCoords(1,2,1,5))  //atsakymas: 3
+console.clear();
 
 //Uzduotis #4:
-// console.log("Uzduotis #4:")
+console.log("Uzduotis #4:")
 
 
 //Uzduotis #5:
